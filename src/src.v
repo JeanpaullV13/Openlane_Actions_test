b1d7d9b9f34d94dad2ad37c515ae14b07477dd5b
Jeanpaull Valencia Quintero <128187857+JeanpaullV13@users.noreply.github.com>
2023-11-09 15:22:06 -0500

    Add files via upload

--- src/src.v
+++ src/src.v
@@ -1,60 +1,77 @@
-module src(
-    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
-    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to 
-    
-    );
-    
-    wire [2:0] bits ;
+`timescale 1ns / 1ps   ///se coloco el tiempo de escalado por warning
+//Micro - Architecture ALU
+// Jeanpaull Valencia Quintero - 2200496
+// Carlos Humberto Diaz Salazar - 2182353
+module ALU #(parameter n = 32)(
+input [n-1:0]SrcA, //input definitions : SrcA and SrcB coming from the Register output data signals
+input [n-1:0]SrcB,
+input [2:0]ALUControl, // ALU control comes from the main controller
+output Zero,                                                                                                       //add new wire
+output reg [n-1:0]ALUResult //ALU result gives us the result depending on the operation
+);
 
-    reg [7:0] cnt;
-    wire [7:0] duty;
-    reg pwm_q,ppm_q;
-    reg [2:0] bits_pre;
-    wire pwm_d,ppm_d;
+// Wire declarations, the main reason of the wire declarations is to provide organization to the code
 
-    assign duty = ui_in [7:0];
-    assign bits = uio_in[2:0];
+wire [n-1:0]A;
+wire [n-1:0]B;
 
-    assign uio_oe = 8'b11111000;
-    assign uio_out[7:0] = 8'b00000000;
-    assign uo_out[7:1] = 7'b0000000;
-    
-    always @(posedge clk) begin
-      if(rst_n) begin
-        bits_pre <= bits;
-        if(bits_pre != bits) begin
-            cnt <= 8'd0;
-            pwm_q <= 1'b0;
-        end
-        else begin
-            
-            pwm_q <= pwm_d;
-                     
-            if((cnt >= (2**bits)))
-                cnt <= 0;
-            else begin     
-                cnt <= cnt + 1;
-            end
-        end
-        bits_pre <= bits;
-      end else begin
-         pwm_q <= 1'b0;
-         cnt <= 0;
-         bits_pre <= bits;
-      end
-    end
+wire [n-1:0]AND; //Main structures
+wire [n-1:0]OR;
+wire [n-1:0]XOR;
+wire [n-1:0]ADD;
+wire [n-1:0]SLT;
 
-    
-    assign pwm_d = (cnt < duty);
-    assign uo_out[0] = pwm_q;
-    
+wire [n-1:0]POST_B; //Support structures
+wire PRE_SLT;
+wire POST_SLT;
 
 
-endmodule
+assign A = SrcA;
+assign B = SrcB;
 
+//Zero
+assign   Zero = (ALUControl[0]==1 & A == B);
+// AND STRUCTURE
+
+assign AND[n-1:0] = A & B;
+
+// OR STRUCTURE
+
+assign OR[n-1:0] = A | B;
+
+// XOR STRUCTURE
+
+assign XOR[n-1:0] = A ^ B;
+
+// B signal modifier
+
+assign POST_B[n-1:0] = ALUControl[0]? ~B + 32'd1:B;
+
+// ADDITION AND SUBSTRACTION STRUCTURE
+
+assign ADD[n-1:0] = A+POST_B; // It's controlled by the LSB of the ALU Controller signal
+
+// SLT signal modifier
+
+assign PRE_SLT = (~ALUControl[1]) & (ADD[n-2] ^ A[n-2]) & ~(A[n-2] ^ B[n-2] ^ ALUControl[0]);
+assign POST_SLT = PRE_SLT ^ ADD[n-2];
+
+// SET LESS THAN
+
+assign SLT[n-1:0] = {31'd0,POST_SLT};
+
+// MUX
+
+always @(*)
+case (ALUControl)
+3'd0: ALUResult = ADD;
+3'd1: ALUResult = ADD;
+3'd2: ALUResult = AND;
+3'd3: ALUResult = OR;
+3'd4: ALUResult = XOR;
+3'd5: ALUResult = SLT;
+default: ALUResult = 32'd0;
+
+endcase
+
+endmodule 
